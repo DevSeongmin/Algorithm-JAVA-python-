f5d2fde2456cb91c850c7879bf2b1ffc08b4a951
devSeongMin <hsm990423@gmail.com>
2025-02-06 11:52:34 +0900

    leet_code esay   Maximum Difference Between Even and Odd Frequency I

---
+++ JAVA/src/leet_code/esay/Maximum_Difference_Between_Even_and_Odd_Frequency_I.java
@@ -0,0 +1,33 @@
+package leet_code.esay;
+
+import java.util.HashMap;
+
+public class Maximum_Difference_Between_Even_and_Odd_Frequency_I {
+	class Solution {
+		public int maxDifference(String s) {
+
+			HashMap<Character, Integer> map = new HashMap();
+
+			for (int i = 0; i < s.length(); i++) {
+				char c = s.charAt(i);
+				map.put(c, map.getOrDefault(c, 0) + 1);
+			}
+
+			System.out.println(map);
+
+			int odd = 0;
+			int even = Integer.MAX_VALUE;
+
+			for (char c : map.keySet()){
+				int freq = map.get(c);
+				if (freq % 2 == 1) {
+					odd = Math.max(odd, freq);
+				} else {
+					even = Math.min(even, freq);
+				}
+			}
+
+			return odd - even;
+		}
+	}
+}
