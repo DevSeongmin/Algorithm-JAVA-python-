75b7ed2a91ebc82b2cdace3a19f920b22219a011
devSeongMin <hsm990423@gmail.com>
2025-03-03 15:51:43 +0900

    leet_code medium 437.Path Sum III

---
+++ JAVA/src/leet_code/medium/_437_Path_Sum_III.java
@@ -0,0 +1,18 @@
+package leet_code.medium;
+
+public class _437_Path_Sum_III {
+	class Solution {
+		public int pathSum(TreeNode root, int targetSum) {
+			if (root == null) return 0;
+			return pathSum(root.left, targetSum) + pathSum(root.right, targetSum) + dfs(root, targetSum);
+
+		} static int dfs(TreeNode node, long targetSum) {
+			if (node == null) return 0;
+			int cnt = 0;
+			if (node.val == targetSum) cnt++;
+			cnt += dfs(node.left, targetSum - node.val);
+			cnt += dfs(node.right, targetSum - node.val);
+			return cnt;
+		}
+	}
+}
