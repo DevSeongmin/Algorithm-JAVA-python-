0a3618bada2e973cb89b928ffc8b949a88c6be4f
devSeongMin <hsm990423@gmail.com>
2025-03-03 14:07:50 +0900

    leet_code medium 1448.Count Good Nodes in Binary Tree

---
+++ JAVA/src/leet_code/medium/_1448_Count_Good_Nodes_in_Binary_Tree.java
@@ -0,0 +1,22 @@
+package leet_code.medium;
+
+public class _1448_Count_Good_Nodes_in_Binary_Tree {
+	class Solution {
+		int good;
+		public int goodNodes(TreeNode root) {
+			good = 0;
+			dfs(root, Integer.MIN_VALUE);
+			return good;
+		}
+		void dfs(TreeNode root, int max) {
+			if (root == null) return;
+			if (max <= root.val) {
+				good++;
+				max = root.val;
+			}
+
+			dfs(root.left, max);
+			dfs(root.right, max);
+		}
+	}
+}
