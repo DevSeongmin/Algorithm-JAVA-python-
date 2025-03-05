d11469b6f76a5ac20ab5d514fed80d56a6a03bdb
devSeongMin <hsm990423@gmail.com>
2025-03-05 14:00:39 +0900

    leet_code medium 1372.Longest ZigZag Path in a Binary Tree

---
+++ JAVA/src/leet_code/medium/_1372_Longest_ZigZag_Path_in_a_Binary_Tree.java
@@ -0,0 +1,25 @@
+package leet_code.medium;
+
+public class _1372_Longest_ZigZag_Path_in_a_Binary_Tree {
+	class Solution {
+		int maxStep = 0;
+		public int longestZigZag(TreeNode root) {
+			dfs(root, -1, 0);
+			dfs(root, 1, 0);
+
+			return maxStep;
+		}
+		private void dfs(TreeNode root, int direction, int step) {
+			if (root == null) return;
+			maxStep = Math.max(maxStep, step);
+
+			if (direction == -1) {
+				dfs(root.right, 1, step + 1);
+				dfs(root.left, -1, 1);
+			} else {
+				dfs(root.left, -1, step + 1);
+				dfs(root.right, 1, 1);
+			}
+		}
+	}
+}
