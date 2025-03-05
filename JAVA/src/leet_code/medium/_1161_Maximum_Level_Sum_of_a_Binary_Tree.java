ec16433b68f1dc76a9c9d23d7c67a1a00c9a104d
devSeongMin <hsm990423@gmail.com>
2025-03-05 14:09:17 +0900

    leet_code medium 1161.Maximum Level Sum of a Binary Tree

---
+++ JAVA/src/leet_code/medium/_1161_Maximum_Level_Sum_of_a_Binary_Tree.java
@@ -0,0 +1,35 @@
+package leet_code.medium;
+
+import java.util.HashMap;
+
+public class _1161_Maximum_Level_Sum_of_a_Binary_Tree {
+	class Solution {
+		static HashMap<Integer, Integer> map;
+
+		public int maxLevelSum(TreeNode root) {
+			map = new HashMap<>();
+
+			dfs(root, 1);
+
+			int answer = -1;
+			int val = Integer.MIN_VALUE;
+
+			for (Integer key : map.keySet()) {
+				if (map.get(key) > val) {
+					answer = key;
+					val = map.get(key);
+				}
+			}
+
+			return answer;
+		}
+		static void dfs(TreeNode root,int depth){
+			if (root == null) return;
+
+			map.put(depth, map.getOrDefault(depth, 0) + root.val);
+
+			dfs(root.left, depth + 1);
+			dfs(root.right, depth + 1);
+		}
+	}
+}
