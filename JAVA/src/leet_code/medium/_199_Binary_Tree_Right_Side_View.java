2dd30c53fe1c2696e61b1ac5713473c34aed4b67
devSeongMin <hsm990423@gmail.com>
2025-03-05 12:28:41 +0900

    leet_code medium 199.Binary Tree Right Side View

---
+++ JAVA/src/leet_code/medium/_199_Binary_Tree_Right_Side_View.java
@@ -0,0 +1,48 @@
+package leet_code.medium;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class _199_Binary_Tree_Right_Side_View {
+
+	class Solution {
+		public List<Integer> rightSideView(TreeNode root) {
+			HashSet<Integer> visited = new HashSet<>();
+			List<Integer> answer = new ArrayList<>();
+
+			Queue<CustomTreeNode> q = new LinkedList<>();
+			q.add(new CustomTreeNode(0, root));
+
+			while(!q.isEmpty()) {
+				CustomTreeNode ctn = q.poll();
+				int step = ctn.step;
+				TreeNode node = ctn.node;
+
+				if (node == null) continue;
+
+				if (!visited.contains(step)){
+					answer.add(node.val);
+					visited.add(step);
+				}
+
+				q.add(new CustomTreeNode(step + 1, node.right));
+				q.add(new CustomTreeNode(step + 1, node.left));
+			}
+
+			return answer;
+		}
+	}
+}
+
+class CustomTreeNode extends TreeNode{
+	int step;
+	TreeNode node;
+
+	public CustomTreeNode(int step, TreeNode node){
+		this.step = step;
+		this.node = node;
+	}
+}
