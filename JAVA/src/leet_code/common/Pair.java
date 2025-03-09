12ade05e406ef1b51b51e66a7fb5bb108f4bd7a2
devSeongMin <hsm990423@gmail.com>
2025-03-09 14:50:44 +0900

    leet_code Node medium 2542.Maximum Subsequence Score

---
+++ JAVA/src/leet_code/common/Pair.java
@@ -0,0 +1,9 @@
+package leet_code.common;
+
+public class Pair {
+	public int a, b;
+	public Pair(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+}
--- JAVA/src/leet_code/medium/_2542_Maximum_Subsequence_Score.java
+++ JAVA/src/leet_code/medium/_2542_Maximum_Subsequence_Score.java
@@ -3,34 +3,30 @@ package leet_code.medium;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 
+
 public class _2542_Maximum_Subsequence_Score {
-	class Pair {
-		int a, b;
-		public Pair(int a, int b) {
-			this.a = a;
-			this.b = b;
-		}
-	}
+
 
 	class Solution {
 		public long maxScore(int[] nums1, int[] nums2, int k) {
 
 			int l = nums1.length;
-			Pair[] pairArr = new Pair[l];
+			leet_code.common.Pair[] pairArr = new leet_code.common.Pair[l];
+
 
 			for (int i = 0; i < l; i++) {
-				pairArr[i] = new Pair(nums1[i], nums2[i]);
+				pairArr[i] = new leet_code.common.Pair(nums1[i], nums2[i]);
 			}
 
 			Arrays.sort(pairArr, (a,b) -> a.b == b.b ? Integer.compare(b.a, a.a) : Integer.compare(b.b, a.b));
 
-			PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.a, b.a));
+			PriorityQueue<leet_code.common.Pair> pq = new PriorityQueue<>((a, b) -> Integer.compare(a.a, b.a));
 
 			long answer = 0;
 			long minProduct = Long.MAX_VALUE;
 			long sum = 0;
 
-			for (Pair p : pairArr) {
+			for (leet_code.common.Pair p : pairArr) {
 				minProduct = Math.min(minProduct, p.b);
 				sum += p.a;
 				pq.add(p);
