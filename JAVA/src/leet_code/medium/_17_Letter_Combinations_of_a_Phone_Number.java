6689a9574f5b0d34b0ab4200cde29c4ecf917db4
devSeongMin <hsm990423@gmail.com>
2025-03-10 15:57:40 +0900

    leet_code medium 17.Letter Combinations of a Phone Number

--- JAVA/src/leet_code/medium/_17_Letter_Combinations_of_a_Phone_Number.java
+++ JAVA/src/leet_code/medium/_17_Letter_Combinations_of_a_Phone_Number.java
@@ -10,23 +10,23 @@ public class _17_Letter_Combinations_of_a_Phone_Number {
 		static StringBuilder sb;
 		static int l;
 		static String digits;
-		static HashMap<Character, String> map = new HashMap<>();
+		static HashMap<Character, String> MAP = new HashMap<>();
 		static {
-			map.put('2', "abc");
-			map.put('3', "def");
-			map.put('4', "ghi");
-			map.put('5', "jkl");
-			map.put('6', "mno");
-			map.put('7', "pqrs");
-			map.put('8', "tuv");
-			map.put('9', "wxyz");
+			MAP.put('2', "abc");
+			MAP.put('3', "def");
+			MAP.put('4', "ghi");
+			MAP.put('5', "jkl");
+			MAP.put('6', "mno");
+			MAP.put('7', "pqrs");
+			MAP.put('8', "tuv");
+			MAP.put('9', "wxyz");
 		}
 
 		public List<String> letterCombinations(String digits) {
 			answer = new ArrayList<>();
 			sb = new StringBuilder();
 			l = digits.length();
-			this.digits = digits;
+			Solution.digits = digits;
 
 			combi(0);
 
@@ -34,18 +34,19 @@ public class _17_Letter_Combinations_of_a_Phone_Number {
 		}
 		static void combi(int depth) {
 
-			if (depth == l && !sb.toString().equals("")) {
+			if (depth == l && !sb.isEmpty()) {
 				answer.add(sb.toString());
 				return;
 			}
 
 			if (depth >= l) return;
 
-			for (int i = 0; i < map.get(digits.charAt(depth)).length(); i++) {
-				sb.append(map.get(digits.charAt(depth)).charAt(i));
+			for (int i = 0; i < MAP.get(digits.charAt(depth)).length(); i++) {
+				sb.append(MAP.get(digits.charAt(depth)).charAt(i));
 				combi(depth+1);
 				sb.deleteCharAt(sb.length() - 1);
 			}
 		}
 	}
+
 }
