0963e09a40382a94f9a1074fc595a49559008a10
DevSeongmin <hsm990423@gmail.com>
2025-05-01 15:27:11 +0900

    FIX leet_code common Node

--- JAVA/src/leet_code/common/Node.java
+++ JAVA/src/leet_code/common/Node.java
@@ -1,7 +1,13 @@
 package leet_code.common;
 
 public class Node {
-	public int y, x, cnt;
+	public int y, x, cnt, val;
+	public Node next;
+	public Node random;
+
+	public Node(int val) {
+		this.val = val;
+	}
 
 	public Node(int y, int x, int cnt) {
 		this.y = y;
