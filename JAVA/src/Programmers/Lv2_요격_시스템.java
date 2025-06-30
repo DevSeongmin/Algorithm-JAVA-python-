80d74d09ac18a29cd0894e00fcd06856310606d6
DevSeongmin <hsm990423@gmail.com>
2025-06-30 09:55:54 +0900

    프로그래머스 Lv2 요격 시스템

--- JAVA/src/Programmers/Lv2_요격_시스템.java
+++ JAVA/src/Programmers/Lv2_요격_시스템.java
@@ -8,14 +8,14 @@ public class Lv2_요격_시스템 {
 			Arrays.sort(targets, (a, b) -> Integer.compare(a[1], b[1]));
 
 			int answer = 1;
-			double shoot = targets[0][1] - 0.1;
+			int shoot = targets[0][1];
 
 			for (int i = 1; i < targets.length; i++) {
 				int[] target = targets[i];
 
-				if (target[0] > shoot) {
+				if (target[0] >= shoot) {
 					answer++;
-					shoot = target[1] - 0.1;
+					shoot = target[1];
 				}
 			}
 			return answer;
