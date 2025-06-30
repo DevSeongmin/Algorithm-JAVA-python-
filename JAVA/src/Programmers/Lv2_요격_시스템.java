a3f53c904e7e10497823c6340bea65fbdb272691
DevSeongmin <hsm990423@gmail.com>
2025-06-30 09:52:25 +0900

    프로그래머스 Lv2 요격 시스템

---
+++ JAVA/src/Programmers/Lv2_요격_시스템.java
@@ -0,0 +1,24 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class Lv2_요격_시스템 {
+	class Solution {
+		public int solution(int[][] targets) {
+			Arrays.sort(targets, (a, b) -> Integer.compare(a[1], b[1]));
+
+			int answer = 1;
+			double shoot = targets[0][1] - 0.1;
+
+			for (int i = 1; i < targets.length; i++) {
+				int[] target = targets[i];
+
+				if (target[0] > shoot) {
+					answer++;
+					shoot = target[1] - 0.1;
+				}
+			}
+			return answer;
+		}
+	}
+}
